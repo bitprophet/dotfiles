1bba889af527203f45646c9f230d90d0dadd229a
Jeff Forcier <jeff@bitprophet.org>
2016-04-10 10:33:42 -0700

    Deal with bizarre-ass new double mouse events

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -29,11 +29,18 @@ end
 -- TODO: check for the KVM switch (USB hub) too, maybe
 hs.usb.watcher.new(function(data)
     mouse = "Evoluent VerticalMouse 4"
-    if data['eventType'] == "added" and data['productName'] == mouse then
-        print("Totally just got a new mouse")
+    -- Need to scan prod/vendor IDs too now, getting 2x connect events for some
+    -- bizarro reason :(
+    prodID = 401
+    vendorID = 6780
+    if data['eventType'] == "added" and data['productName'] == mouse
+    and data['productID'] == prodID and data['vendorID'] == vendorID
+    then
+        print("Received new EVM4 event: ")
+        print(hs.inspect(data))
+        print("")
         -- Tickle mouse acceleration up, then down
         tickleMouseAccel()
-        print("Successfully executed tickleMouseAccel()")
     end
 end):start()
 
