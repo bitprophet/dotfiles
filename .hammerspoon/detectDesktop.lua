de15e6a4b5dc931d1083e284676bc8f9f2713c26
Jeff Forcier <jeff@bitprophet.org>
2019-06-01 14:15:25 -0400

    Try to fix weird new behavior re: natural scrolling
    See comment in the code for deets

--- .hammerspoon/detectDesktop.lua
+++ .hammerspoon/detectDesktop.lua
@@ -23,20 +23,35 @@ function callWhenScreenChanges()
         log.d("Received new screen-change event!")
         log.d("Printing current list o screens:")
         log.d(hs.inspect(hs.screen.allScreens()))
-        -- Above seems to be a dict/array as expected, when on TB it is just TB
-        -- display, when on internal it is "Color LCD". Whee
-        -- TODO: useNaturalScrolling(true) when screen has changed to be just
-        -- internal display
-        -- Wow, why the hell is Lua so fucking popular again? This is some dumb
-        -- bullshit.
+        -- Above seems to be a dict/array as expected, when on LG it's "LG
+        -- ULTRAWIDE", when on internal it is "Color LCD". Whee.
+        -- Wow, why the hell is Lua so fucking popular again? This crappy
+        -- version of "if x in y" is some dumb bullshit.
         current_screen = nil
         count = 0
         for k, v in pairs(hs.screen.allScreens()) do
             current_screen = v
             count = count + 1
         end
-        if count == 1 and current_screen:name() == "Color LCD" then
-            useNaturalScrolling(true)
+        if count == 1 then
+            -- TODO: new behavior due to one or more of the following factors:
+            -- - MacBook non-Pro 2018
+            -- - use of CalDigit Thunderbolt 3 dock
+            -- - LG ULTRAWIDE external display
+            -- What happens is:
+            -- - Docked, display has slept
+            -- - Wake up with keyboard/mouse
+            -- - Screen change event fires w/ Color LCD present only
+            -- - 4-5 seconds later, ANOTHER fires w/ LG present only
+            -- For now, just adapted this to say "when not color LCD, go back
+            -- to unnatural scrolling" which may result in extraneous prefpane
+            -- manipulation, but should prevent the above bug where we were
+            -- setting natural scrolling on incorrectly...
+            if current_screen:name() == "Color LCD" then
+                useNaturalScrolling(true)
+            else
+                useNaturalScrolling(false)
+            end
         end
     end)
 end
