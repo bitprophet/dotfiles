34061e6cce0d53186f24078658bbcd29b4dd390c
Jeff Forcier <jeff@reach.security>
2025-01-28 19:27:38 -0500

    Start tacking on some brew installs I guess

--- setup.sh
+++ setup.sh
@@ -12,3 +12,31 @@ vim '+PlugUpdate | qa!'
 # it's least-bad to let it run normally (async but w/ output visible) at cost
 # of having to manually quit vim after (the horror!)
 vim "+CocInstall ${COC_EXTENSIONS}"
+
+# And fuckit, I guess we're just going back to the early 2000s, config
+# management via shell script.
+if which brew &>/dev/null; then
+    typeset -a FORMULAE
+    FORMULAE=(
+        asciinema
+        bat
+        btop
+        fswatch
+        fzf
+        gh
+        # sometimes git, depending on what version macOS shipped
+        httpie
+        iperf3
+        jq
+        lazygit
+        pyenv
+        tmux
+        tree
+        vim
+        wget
+        # sometimes zsh, ditto
+    )
+    brew install $FORMULAE
+fi
+
+
