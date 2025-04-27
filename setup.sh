8b4513842ebb1ac0a9f0272da3121f4a4df7e309
Jeff Forcier <jeff@bitprophet.org>
2025-04-27 19:14:59 -0400

    Setup.sh now bootstraps hemera? mostly?

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # vim: set ft=zsh
 
-# Exit on error.
-set -e
+# Exit (return) on error.
+set -o ERR_RETURN
 
 # Minimum viable bootstrap to run Hemera. UV makes this a little easier now! as
 # do my dotfiles, which set $PLATFORM based on whatever heuristics.
@@ -13,7 +13,6 @@ case $PLATFORM in
         return 1
         ;;
     mac )
-        brew install uv
         # TODO: I /could/ set up hemera to be a truly /installable/ python
         # project with a CLI entrypoint, then aim uv at it like so:
         #
@@ -22,16 +21,33 @@ case $PLATFORM in
         #
         # which would use an ephemeral venv to execute.
         # TODO: however that got a bit messy fast and it's not like I /don't/
-        # want to end up with a real project checkout eventually, so this just
+        # want to end up with a real project checkout eventually, so below just
         # duplicates a teensy bit of what Hemera does internally re: such
         # checkouts...probably fine for now? esp given how infrequently I set
-        # up new macs?
-        _CHECKOUT=~/Code/personal
-        mkdir -p $_CHECKOUT
-        git clone git@github.com:bitprophet/hemera ${_CHECKOUT}/hemera
+        # up new macs? ehhh we'll see, if it drifts too much or too fast I'll
+        # reconsider!
+
+        # Reuse some bits (as this is often run in a fresh shell)
+        source ~/.zsh/python
+        CHECKOUT=~/Code/personal/hemera
+        REPO="git@github.com:bitprophet/hemera"
+        export UV_PROJECT_ENVIRONMENT="${WORKON_HOME}/hemera"
+
+        brew install uv
         echo
-        wk hemera
-        inv mac
+        mkdir -p $(dirname $CHECKOUT)
+        git clone $REPO $CHECKOUT
+        echo
+
+        cd $CHECKOUT
+        # --seed to inject pip
+        uv venv --seed
+        # Hemera has a pyproject.toml now
+        uv sync
+        # Easier to run in my regular shell env or I'll periodically violate
+        # various assumptions...
+        echo
+        echo "All done! Please execute 'wk hemera && inv mac'."
         ;;
 esac
 
